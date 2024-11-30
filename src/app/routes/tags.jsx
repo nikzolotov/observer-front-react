d99caa7659c8f7715af4c3cf146964289a0ee2cb
Nikita Zolotov <nikzolotov@gmail.com>
2024-11-30 09:49:58 +0300

    Filter observation by multiple tags

--- src/app/routes/tags.jsx
+++ src/app/routes/tags.jsx
@@ -1,34 +1,79 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 import qs from "qs";
 
 export const tagsLoader = async ({ params }) => {
   const slugsArray = params.slugs.split(",");
-  const query = qs.stringify({
-    sort: "createdAt:asc",
-    filters: {
-      slug: {
-        $in: slugsArray,
+
+  const query = qs.stringify(
+    {
+      sort: "createdAt:asc",
+      filters: {
+        slug: {
+          $in: slugsArray,
+        },
       },
     },
-  });
-
-  const response = await fetch(
-    `${import.meta.env.VITE_STRAPI_API_URL}tags?${query}`
+    {
+      encodeValuesOnly: true, // prettify URL
+    }
   );
-  const data = await response.json();
+
+  const observationsQuery = qs.stringify(
+    {
+      // filters: {
+      //   tags: {
+      //     $and: slugsArray.map((slug) => ({
+      //       slug: {
+      //         $eq: slug,
+      //       },
+      //     })),
+      //   },
+      // },
+      filters: {
+        $and: slugsArray.map((slug) => ({
+          tags: {
+            slug: {
+              $eq: slug,
+            },
+          },
+        })),
+      },
+      populate: "media",
+    },
+    {
+      encodeValuesOnly: true, // prettify URL
+    }
+  );
+
+  console.log(observationsQuery);
+
+  const [tagsResponse, observationsResponse] = await Promise.all([
+    fetch(`${import.meta.env.VITE_STRAPI_API_URL}tags?${query}`),
+    fetch(
+      `${import.meta.env.VITE_STRAPI_API_URL}observations?${observationsQuery}`
+    ),
+  ]);
+
+  const [tagsData, observationsData] = await Promise.all([
+    tagsResponse.json(),
+    observationsResponse.json(),
+  ]);
+
   return {
-    tags: data.data,
+    tags: tagsData.data,
+    observations: observationsData.data,
   };
 };
 
 export const Tags = () => {
-  const { tags } = useLoaderData();
+  const { tags, observations } = useLoaderData();
   return (
     <>
       <h1>
         <TmpTitle data={tags} />
       </h1>
-      <div>Here goes observations</div>
+      <ObservationList data={observations} />
+      <dib>{observations.length}</dib>
     </>
   );
 };
@@ -44,5 +89,38 @@ function TmpTitle({ data }) {
         </span>
       ))}
     </>
+  );
+}
+
+function ObservationList({ data }) {
+  return (
+    <ul className="observations">
+      {data.map((node) => (
+        <ObservationListItem data={node} key={node.id} />
+      ))}
+    </ul>
+  );
+}
+
+function ObservationListItem({ data }) {
+  return (
+    <li className="observations__item">
+      <Link to={`observation/${data.id}`}>
+        <img
+          width={200}
+          className="observations__image"
+          src={
+            data.media[0]
+              ? `${
+                  import.meta.env.VITE_STRAPI_UPLOADS +
+                  data.media[0].hash +
+                  data.media[0].ext
+                }`
+              : ""
+          }
+        />
+      </Link>
+      <p className="observations__year">{data.createdAt}</p>
+    </li>
   );
 }
